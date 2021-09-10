d0675fb981b46cc8462b4ef970bf1c3d50929af6
Alexander0076 <uliseselias007@gmail.com>
2021-09-10 17:50:36 -0600

    Ejercicio 3 completo

--- Tarea/Tarea/Ejercicio3.Designer.cs
+++ Tarea/Tarea/Ejercicio3.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.BtnAtras = new System.Windows.Forms.Button();
-            this.TxtNumA = new System.Windows.Forms.TextBox();
+            this.TxtCantA = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.Data = new System.Windows.Forms.DataGridView();
@@ -38,8 +38,8 @@
             // 
             // BtnAtras
             // 
-            this.BtnAtras.Location = new System.Drawing.Point(11, 319);
-            this.BtnAtras.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnAtras.Location = new System.Drawing.Point(-1, 389);
+            this.BtnAtras.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.BtnAtras.Name = "BtnAtras";
             this.BtnAtras.Size = new System.Drawing.Size(111, 19);
             this.BtnAtras.TabIndex = 0;
@@ -47,18 +47,18 @@
             this.BtnAtras.UseVisualStyleBackColor = true;
             this.BtnAtras.Click += new System.EventHandler(this.BtnAtras_Click);
             // 
-            // TxtNumA
+            // TxtCantA
             // 
-            this.TxtNumA.Location = new System.Drawing.Point(187, 38);
-            this.TxtNumA.Margin = new System.Windows.Forms.Padding(2);
-            this.TxtNumA.Name = "TxtNumA";
-            this.TxtNumA.Size = new System.Drawing.Size(117, 20);
-            this.TxtNumA.TabIndex = 1;
+            this.TxtCantA.Location = new System.Drawing.Point(189, 65);
+            this.TxtCantA.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.TxtCantA.Name = "TxtCantA";
+            this.TxtCantA.Size = new System.Drawing.Size(117, 20);
+            this.TxtCantA.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(11, 41);
+            this.label1.Location = new System.Drawing.Point(170, 36);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(155, 13);
@@ -67,8 +67,8 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(342, 31);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Location = new System.Drawing.Point(211, 102);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(76, 33);
             this.button1.TabIndex = 3;
@@ -79,8 +79,8 @@
             // Data
             // 
             this.Data.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.Data.Location = new System.Drawing.Point(36, 79);
-            this.Data.Margin = new System.Windows.Forms.Padding(2);
+            this.Data.Location = new System.Drawing.Point(121, 160);
+            this.Data.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Data.Name = "Data";
             this.Data.RowTemplate.Height = 24;
             this.Data.Size = new System.Drawing.Size(268, 211);
@@ -90,13 +90,13 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(439, 366);
+            this.ClientSize = new System.Drawing.Size(500, 433);
             this.Controls.Add(this.Data);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.TxtNumA);
+            this.Controls.Add(this.TxtCantA);
             this.Controls.Add(this.BtnAtras);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Ejercicio3";
             this.Text = "Ejercicio3";
             ((System.ComponentModel.ISupportInitialize)(this.Data)).EndInit();
@@ -108,7 +108,7 @@
         #endregion
 
         private System.Windows.Forms.Button BtnAtras;
-        private System.Windows.Forms.TextBox TxtNumA;
+        private System.Windows.Forms.TextBox TxtCantA;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView Data;
--- Tarea/Tarea/Ejercicio3.cs
+++ Tarea/Tarea/Ejercicio3.cs
@@ -24,40 +24,48 @@ namespace Tarea
             Form1 form = new Form1();
             form.Show();
         }
-        
-        private void button1_Click(object sender, EventArgs e)
+        private void asignacion(int numero)
         {
+            int alumno = numero;
             int x = 1;
-            int[] vector = new int[20];
-            int nombre = Convert.ToInt32(TxtNumA.Text);
-            int[] lista = new int[nombre];
-            if (nombre <= 10)
-            {
+            int[] lista = new int[alumno];
+            int[] N_impar = new int[20];
+            
                 Data.Columns.Add("Alumnos", "Alumnos");
                 Data.Columns.Add("Pupitre", "Pupitre");
-                for (int i = 0; i < nombre; i++)
+                for (int i = 0; i < alumno; i++)
                 {
                     lista[i] = x;
                     x++;
                 }
                 int z = 1;
-                for (int i = 0; i < nombre; i++)
+                for (int i = 0; i < alumno; i++)
                 {
-                    for (int j = 0; j < nombre; j++)
+                    for (int j = 0; j < alumno; j++)
                     {
 
-                        vector[i] = z;
+                        N_impar[j] = z;
                         Data.Rows.Add(lista[i], z);
                         z += 2;
                         break;
 
                     }
                 }
-                TxtNumA.Clear();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+            int cantidad = Convert.ToInt32(TxtCantA.Text);
+            if (cantidad <= 10)
+            {
+                asignacion(cantidad);
+                TxtCantA.Clear();
             }
             else
             {
-                MessageBox.Show("La cantidad de alumnos no debe ser mayor a 10", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("La cantidad maxima es de 10 alumnos por salón, porfavor ingrese numero que este en el rango establecido", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
            
         }
